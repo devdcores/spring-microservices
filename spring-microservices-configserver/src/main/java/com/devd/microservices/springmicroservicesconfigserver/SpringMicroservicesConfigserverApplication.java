dfdef40ae7523219f69fdf36ca816c7ec87d8bc4
devaraj reddy <devaraj.reddy@siemens.com>
2018-02-12 00:20:49 +0530

    config server initial commit.

---
+++ spring-microservices-configserver/src/main/java/com/devd/microservices/springmicroservicesconfigserver/SpringMicroservicesConfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.devd.microservices.springmicroservicesconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesConfigserverApplication.class, args);
+	}
+}
