9d952b8c9c14794d239566a52a96b50ac447ee05
devaraj reddy <devaraj.reddy@siemens.com>
2018-02-15 00:36:10 +0530

    config client refactor

---
+++ spring-microservices-configclient/src/main/java/com/devd/microservices/springmicroservicesconfigclient/SpringMicroservicesConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.devd.microservices.springmicroservicesconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesConfigClientApplication.class, args);
+	}
+}
