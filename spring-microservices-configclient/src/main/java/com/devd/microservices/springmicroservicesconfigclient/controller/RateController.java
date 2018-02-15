9f6dd85071ad90819bd7493a540d5b00ab244e91
devaraj reddy <devaraj.reddy@siemens.com>
2018-02-15 09:42:01 +0530

    refresh scope in controller to refresh endpoints

--- spring-microservices-configclient/src/main/java/com/devd/microservices/springmicroservicesconfigclient/controller/RateController.java
+++ spring-microservices-configclient/src/main/java/com/devd/microservices/springmicroservicesconfigclient/controller/RateController.java
@@ -1,6 +1,7 @@
 package com.devd.microservices.springmicroservicesconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Devaraj Reddy on 2/14/2018.
  */
 @Controller
+@RefreshScope
 public class RateController {
 
     @Value("${rate}")
